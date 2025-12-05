372bb3a5bf2e811a372b6bb04867b6437c7eb1fc
usamausman-jsx <theyakki18@gmail.com>
2025-12-05 10:47:13 +0500

    FEAT : Added Mongodb connectivity

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const mongoose = require('mongoose');
 const express = require('express')
 const students = require('./routes/students');
+require('dotenv').config()
 const { logging } = require('./middlewares/logging')
 const app = express()
 const port = 3000
@@ -8,6 +10,11 @@ app.use(express.json());
 app.use(logging);
 app.use('/api', students);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`DB Connected Successfully , app listening on port ${port}`)
+  } catch (error) {
+    console.log(error)
+  }
 })
